a9cee01959baedbdac8687394e7c0cb7c82f2a0f
Alexander Moryan <a.dencyh@gmail.com>
2022-09-13 22:46:53 +0300

    cleanup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import {createRoot} from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
-// import "./index.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
